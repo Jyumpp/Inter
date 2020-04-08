d8eb8022aad43290040fb626aaf169ac5695ab20
Jyumpp <hdhdog@live.com>
2020-04-08 13:23:29 -0400

    added code this time

--- app/src/main/java/com/example/implementation/Solver.kt
+++ app/src/main/java/com/example/implementation/Solver.kt
@@ -1,4 +1,29 @@
 package com.example.implementation
 
+import com.paramsen.noise.Noise
+
 class Solver {
+    fun solve(samples: Int, rate: Float, src: FloatArray, dst: FloatArray): Float {
+        val noise = Noise.real(samples)
+
+        val fft: FloatArray = noise.fft(src, dst)
+
+        val pairs: MutableList<Pair<Float, Float>> = mutableListOf()
+
+        for (i in 2 until fft.size step 2) {
+            val real = fft[i]
+            val imag = fft[i + 1]
+
+            pairs.add(Pair(real, imag))
+        }
+
+        var greatest: Int = 0
+        for (i in pairs.indices) {
+            if (pairs[i].first > pairs[greatest].first) {
+                greatest = i
+            }
+        }
+
+        return (rate * (greatest + 1) / samples)
+    }
 }
--- app/src/main/java/com/example/implementation/Test.java
+++ app/src/main/java/com/example/implementation/Test.java
@@ -1,4 +1,20 @@
 package com.example.implementation;
 
 public class Test {
+    public static void main(String[] args) {
+        Solver s = new Solver();
+
+        float freq = 87.35f;
+        float rate = 44000f;
+        int samples = 44000;
+
+        float[] src = new float[samples];
+        float[] dst = new float[samples + 2];
+
+        for (int i = 0; i < samples; i++) {
+            src[i] = (float)Math.cos(2 * Math.PI * i * freq / rate);
+        }
+
+        System.out.println(s.solve(samples, rate, src, dst));
+    }
 }
